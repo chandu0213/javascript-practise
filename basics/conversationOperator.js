9138553e392325543e54da5cf312334bcac52f47
chandu0213 <edupachandu17@gmail.com>
2025-07-03 12:26:18 +0000

    convrsation of datatypes

---
+++ basics/conversationOperator.js
@@ -0,0 +1,16 @@
+let score = "33djs"
+
+console.log(typeof score);
+console.log(typeof Number(score) );
+
+let valuenumber = Number(score)
+console.log(valuenumber);
+
+let login =  1
+
+let logout = Boolean(login)
+console.log(logout);
+
+// 1=> "true" 0=> "false"
+// "" => false
+// "chandu" => true
